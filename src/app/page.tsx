41c5ebdd3084385f0b4e375d7c87c8354269fb5e
Jilmar <jilmar141@gmail.com>
2025-07-31 11:12:14 -0400

    ultimo cambio desde linux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
     <div>
       <h1 className="text-3xl font-bold">Aqui esta la modificacion</h1>
       <Button color="warning">Click me</Button>
+      <p className="text-lg text-red-400">Aqui vamos de nuevo desde linux</p>
     </div>
   );
 }
