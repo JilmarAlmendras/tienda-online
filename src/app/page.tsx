166b7d5530d220c2dab5cb35d3dc77e303620297
Jilmar <jilmar141@gmail.com>
2025-07-31 11:09:46 -0400

    prueba desde linux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@heroui/react";
 export default function HomePage() {
   return (
     <div>
+      <h1 className="text-3xl font-bold">Aqui esta la modificacion</h1>
       <Button color="warning">Click me</Button>
     </div>
   );
